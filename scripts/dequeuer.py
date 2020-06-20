b43c5b963981b9b8a59fc201ce0e48d77108476d
Alan Sammarone <alansammarone@gmail.com>
2020-06-20 12:53:21 +0200

    fix

--- scripts/dequeuer.py
+++ scripts/dequeuer.py
@@ -157,14 +157,11 @@ class Percolation2DSquareStatsProcessor(SQSMessageProcessor):
         return """
             SELECT
                 id,
-                percolation_2d_square_id,
-                size,
                 probability,
-                has_percolated,
-                cluster_size_histogram,
-                average_cluster_size,
-                created,
-                took
+                size,
+                observables,
+                took,
+                created
             FROM percolation_2d_square
             WHERE round(probability, 6) = %(probability)s
             AND size = %(size)s
--- scripts/enqueuer.py
+++ scripts/enqueuer.py
@@ -1,4 +1,5 @@
 from musk.core.sqs import SQSQueue
+from musk.core.sql import MySQL
 
 
 class Percolation2DSquare(SQSQueue):
@@ -9,20 +10,33 @@ class Percolation2DSquareStatsQueue(SQSQueue):
     name = "percolation_2d_square_stats"
 
 
+def get_all_sizes_and_probabilities():
+    query = "select distinct size, probability from percolation_2d_square;"
+    mysql = MySQL()
+    results = mysql.fetch(query)
+    return results
+
+
 type_ = "stats"
 env = "prod"
 
 if type_ == "simulation":
-
-    for p in range(45, 75):
-        template = dict(parameters=dict(probability=p / 100, size=512), repeat=16,)
-        Percolation2DSquare(env).write([template] * 4)
+    for p in range(550, 650):
+        # for p in range(45, 75):
+        template = dict(parameters=dict(probability=p / 1000, size=512), repeat=32,)
+        Percolation2DSquare(env).write([template] * 10)
 
 elif type_ == "stats":
 
-    for p in range(45, 75):
-        # for size in [16, 32, 64]:
-        # for size in [128, 256, 512]:
-        for size in [192]:
-            template = dict(parameters=dict(probability=p / 100, size=size))
-            Percolation2DSquareStatsQueue(env).write([template])
+    combinations = list(get_all_sizes_and_probabilities())
+    import random
+
+    random.shuffle(combinations)
+    for row in combinations:
+        size = row["size"]
+        probability = row["probability"]
+        if size >= 512:
+            continue
+        template = dict(parameters=dict(probability=probability, size=size))
+        print(f"Sending {template}")
+        Percolation2DSquareStatsQueue(env).write([template] * 10)
