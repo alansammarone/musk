421c48e1b5f7dc61b1a8479ddd2d900eac74d0db
Alan Sammarone <alansammarone@gmail.com>
2020-07-22 00:51:51 +0200

    improvement

--- scripts/run_dequeuer.sh
+++ scripts/run_dequeuer.sh
@@ -2,11 +2,12 @@
 
 LOCKFILE=/tmp/dequeuer.lock
 GLOBALLOCKFILE=/tmp/dequeuerglobal.lock
-if [ -f $LOCKFILE ]; then
-	echo "Dequeuer already running."
+if [ -f $GLOBALLOCKFILE ]; then
+	echo "Global lock found."
+	
 else
 	if [ -f $LOCKFILE ]; then
-		echo "Global lock found."
+		echo "Dequeuer already running."
 	else:
 		touch $LOCKFILE
     	../env/bin/python dequeuer_runner.py
