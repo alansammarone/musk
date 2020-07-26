79e585b270d7c572a4778b01f82ac88ef9f61f75
Alan Sammarone <alansammarone@gmail.com>
2020-07-27 01:46:56 +0200

    improvement

--- scripts/enqueuer.py
+++ scripts/enqueuer.py
@@ -14,7 +14,7 @@ from musk.percolation import (
 
 model = "square_2d"
 type_ = "stats"
-env = "dev"
+env = "prod"
 
 
 print(f"Environment: {env.upper()}")
@@ -72,7 +72,7 @@ def get_all_ids_for_size_and_probability(size, probability, limit):
 def get_id_chunks(size, probability):
 
     limit = 8192
-    chunk_size = 256
+    chunk_size = 350
     ids = [
         row["id"]
         for row in get_all_ids_for_size_and_probability(size, probability, limit)
@@ -116,7 +116,8 @@ elif type_ == "stats":
         # "average_correlation_length",
         "percolating_cluster_strength",
     ]
-    size_filter = [16, 32, 96, 128, 192, 256, 294, 512]
+    # size_filter = [16, 32, 96, 128, 192, 256, 294, 512]
+    size_filter = [64]
     probability_filter = detailed_p_2d_range + extension_p_2d_range + general_p_2d_range
     random.shuffle(combinations)
     combinations = filter(lambda comb: comb["size"] in size_filter, combinations)
